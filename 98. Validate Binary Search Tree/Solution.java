e04a0fb4662faf4012eff9bc4fa47d2851841448
Kamal Agarwal <kamal.addicted@gmail.com>
2021-02-03 22:55:14 +0530

    Solution for Validate Binary Search Tree

---
+++ 98. Validate Binary Search Tree/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution
+{
+    public boolean isValidBST( TreeNode root )
+	{
+        return isTreeInRange( root, Integer.MIN_VALUE, Integer.MAX_VALUE );
+    }
+	
+	private static boolean isTreeInRange( TreeNode node, long min, long max )
+	{
+		if( node == null )
+		{
+			return true;
+		}
+		if( node.val < min || node.val > max )
+		{
+			return false;
+		}
+		return isTreeInRange( node.left, min, (long) node.val - 1 ) && isTreeInRange( node.right, (long) node.val + 1, max );
+	}
+}
